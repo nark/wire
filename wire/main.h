9f0c86499ef5a9f23ed0e765c47eb9e3357b64e7
balr0g <balrog032@gmail.com>
2014-04-22 14:27:50 -0400

    Rename .wire to .wire2 to allow running side-by-side with 1.x

--- wire/main.h
+++ wire/main.h
@@ -31,8 +31,8 @@
 
 #include <signal.h>
 
-#define WR_WIRE_PATH					".wire"
-#define WR_WIRE_CONFIG_PATH				".wire/config"
+#define WR_WIRE_PATH					".wire2"
+#define WR_WIRE_CONFIG_PATH				".wire2/config"
 
 enum _wr_completer {
 	WR_COMPLETER_NONE					= 0,
