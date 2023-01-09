524212a8bc848120e039d8086fae03ce890fcf19
Luigi <45099401+ProfDrLuigi@users.noreply.github.com>
2023-01-09 14:16:09 +0100

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -3,6 +3,15 @@
 #
 #### Put this file into your .wire home directory
 
+####################################################
+#### Switch desired function on or off (0 or 1).####
+####################################################
+user_join=1
+user_leave=1
+wordpolice=1
+greeting=1
+####################################################
+
 SELF=$(SELF=$(dirname "$0") && bash -c "cd \"$SELF\" && pwd")
 cd "$SELF"
 
@@ -13,7 +22,7 @@ nick_low=$( echo "$nick" | tr '[:upper:]' '[:lower:]' )
 command=$( cat "$out_file" | sed 's/.*-###-//g' | xargs )
 
 function print_msg {
-  /usr/bin/screen -S wirebot -X stuff "$say"^M
+  /usr/bin/screen -S wirebot -p0 -X stuff "$say"^M
 }
 
 function rnd_answer {
@@ -23,22 +32,65 @@ function rnd_answer {
   print_msg
 }
 
+if [ -f cmd.stop ]; then
+  if [ "$command" = "!start" ]; then
+  	rm cmd.stop
+  elif [ "$command" = "!stop" ]; then
+    say="/afk"
+    print_msg
+    exit
+  fi
+fi  
+
+if [[ "$command" == *"Using timestamp"* ]]; then
+  rm cmd.stop
+fi
+
+#### User join server (user_join) ####
+if [ $user_join = 1 ]; then
+  if [[ "$command" == *" has joined" ]]; then
+    nick=$( cat "$out_file" | sed -e 's/.*\]\ //g' -e 's/\ has\ joined//g' -e 's/;0m//g' | xargs )
+    say="Hi $nick :-)"
+    print_msg
+  fi
+fi
+
+#### User leave server (user_leave) ####
+if [ $user_leave = 1 ]; then
+  if [[ "$command" == *" has left" ]]; then
+    nick=$( cat "$out_file" | sed -e 's/.*\]\ //g' -e 's/\ has\ left//g' -e 's/;0m//g' | xargs )
+    say="Bye $nick :worried:".
+    print_msg
+  fi
+fi
+
+#### Wordpolice (wordpolice)####
 if [[ "$command" == *"Hey, why did you"* ]]; then
   exit
 fi
 
-if [[ "$command" == *" has joined" ]]; then
-  nick=$( cat "$out_file" | sed -e 's/.*\]\ //g' -e 's/\ has\ joined//g' -e 's/;0m//g' | xargs )
-  say="Hi $nick (-:"
-  print_msg
+if [ $wordpolice = 1 ]; then
+  if [ "$command" = "shit" ] || [[ "$command" = *"fuck"* ]] || [ "$command" = "asshole" ] || [ "$command" = "ass" ] || [ "$command" = "dick" ]; then
+    answ[0]="$nick, don't be rude please..."
+    answ[1]="Very impolite!"
+    answ[2]="Hey, why did you say \"$command\" ?"
+    rnd_answer
+    exit
+  fi
 fi
 
-if [[ "$command" == *" has left" ]]; then
-  nick=$( cat "$out_file" | sed -e 's/.*\]\ //g' -e 's/\ has\ left//g' -e 's/;0m//g' | xargs )
-  say="Bye $nick :worried:".
-  print_msg
+#### Greetings (greeting) ####
+if [ $greeting = 1 ]; then
+  if [ "$command" = "hello" ] || [ "$command" = "hey" ] || [ "$command" = "hi" ]; then
+    answ[0]="Hey $nick. :-)"
+    answ[1]="Hello $nick. :-)"
+    answ[2]="Hi $nick. :-)"
+    answ[2]="Yo $nick. :-)"
+    rnd_answer
+  fi
 fi
 
+#### Admin functions ####
 if [[ "$nick_low" == *"luigi"* ]]; then
   if [ "$command" = "!test" ]; then
     say="Weeeeeeeee :blush:"
@@ -64,21 +116,6 @@ if [[ "$nick_low" == *"luigi"* ]]; then
     rnd_answer
     say="/afk"
     print_msg
+    touch cmd.stop
   fi
-fi
-
-if [ "$command" = "shit" ] || [[ "$command" = *"fuck"* ]] || [ "$command" = "asshole" ] || [ "$command" = "ass" ] || [ "$command" = "dick" ]; then
-  answ[0]="$nick, don't be rude please..."
-  answ[1]="Very impolite!"
-  answ[2]="Hey, why did you say \"$command\" ?"
-  rnd_answer
-  exit
-fi
-
-if [ "$command" = "hello" ] || [ "$command" = "hey" ] || [ "$command" = "hi" ]; then
-  answ[0]="Hey $nick. :-)"
-  answ[1]="Hello $nick. :-)"
-  answ[2]="Hi $nick. :-)"
-  answ[2]="Yo $nick. :-)"
-  rnd_answer
 fi
